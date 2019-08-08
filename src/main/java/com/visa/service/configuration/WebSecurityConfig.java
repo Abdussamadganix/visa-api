d9fc830f62f3332dd65efbfdc36f5a0e6b9f9afe
XPRESSPAY\abdussamad.olaiya <abdussamad.olaiya@xpresspayments.com>
2019-08-08 18:09:15 +0100

    initial commit

--- src/main/java/com/visa/service/configuration/WebSecurityConfig.java
+++ src/main/java/com/visa/service/configuration/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.visa.service.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Abdussamad
@@ -18,5 +22,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
         .antMatchers(new String[]{"/"}).permitAll();
     http.csrf().disable();
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("PUT", "DELETE", "POST", "GET")
+            .allowedHeaders("Token", "Authorization", "Content-Type")
+            .allowCredentials(true).maxAge(3600);
+      }
+    };
   }
 }
